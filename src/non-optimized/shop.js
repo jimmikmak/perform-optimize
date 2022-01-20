479933950153399c58112200c626124552d225a9
James McCarron <mccarron.j@gmail.com>
2022-01-20 13:45:01 +1100

    Optimised code changes

--- src/non-optimized/shop.js
+++ src/non-optimized/shop.js
@@ -1,13 +1,13 @@
-import { initProducts } from "./product-management";
+import { initProducts } from './product-management';
 
-const addProduct = (event) => {
-  import("./product-management.js").then((mod) => {
+function addProduct(event) {
+  import('./product-management.js').then(mod => {
     mod.addProduct(event);
-  });
-};
+  })
+}
 
-const addProductForm = document.getElementById("new-product");
+const addProductForm = document.getElementById('new-product');
 
 initProducts();
 
-addProductForm.addEventListener("submit", addProduct);
+addProductForm.addEventListener('submit', addProduct);
--- src/optimized/product-management.js
+++ src/optimized/product-management.js
@@ -1,39 +1,31 @@
-import { renderProducts } from "./rendering";
-import { products as prods } from "./products";
-import { initProducts } from "./product-management";
+import { updateProducts } from './rendering';
+import { products } from './products';
 
-let products = prods;
+const titleEl = document.getElementById('title');
+const priceEl = document.getElementById('price');
 
 export function deleteProduct(prodId) {
-  const updatedProducts = [];
-  for (const prod of products) {
-    if (prod.id !== prodId) {
-      updatedProducts.push(prod);
-    }
-  }
-  products = updatedProducts;
-  renderProducts(products, deleteProduct);
+  const deletedProductIndex = products.findIndex(prod => prod.id === prodId);
+  const deletedProduct = products[deletedProductIndex];
+  products.splice(deletedProductIndex, 1);
+  updateProducts(deletedProduct, prodId, deleteProduct, false);
 }
 
 export function addProduct(event) {
-  event.preventDefault();
-  const titleEl = document.querySelector("#new-product #title");
-  const priceEl = document.querySelector("#new-product #price");
-
   const title = titleEl.value;
   const price = priceEl.value;
 
   if (title.trim().length === 0 || price.trim().length === 0 || +price < 0) {
-    alert("Please enter some valid input values for title and price.");
+    alert('Please enter some valid input values for title and price.');
     return;
   }
 
   const newProduct = {
     id: new Date().toString(),
     title: title,
-    price: price,
+    price: price
   };
 
   products.unshift(newProduct);
-  renderProducts(products, deleteProduct);
+  updateProducts(newProduct, newProduct.id, deleteProduct, true);
 }
